d505cf88b4594f570f061fde681e9b403e0bd550
tomas.svoboda <tomas.svoboda@jamf.com>
2024-05-14 10:32:11 +0200

    add: release v1.1

--- regatta-java-spring-data/build.gradle.kts
+++ regatta-java-spring-data/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.jamf.regatta"
-version = "1.0.1-SNAPSHOT"
+version = "1.1.0"
 
 dependencies {
     api("org.springframework.data:spring-data-keyvalue:$springDataVersion")
